cbd58921879014e3d3386d840eb455514d502857
Romchirik <comaro80@gmail.com>
2023-02-19 18:41:44 +0700

    started work on second part

--- FirstLab/src/main/java/ru/nsu/titov/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.titov;
-
-public class Main {
-    public static void main(String[] args) {
-        var a = new SomeTestClass();
-        a.getSomeField();
-        a.setSomeField(10);
-    }
-}
---
+++ FirstLab/src/main/java/ru/nsu/titov/app/XMLReaderApplication.java
@@ -0,0 +1,12 @@
+package ru.nsu.titov.app;
+
+import java.util.concurrent.Callable;
+
+public class XMLReaderApplication implements Callable<Integer> {
+
+
+    @Override
+    public Integer call() throws Exception {
+
+    }
+}
